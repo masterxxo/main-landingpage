2f5432702dcefafb1864e8b3f4f2dbf32cbf250e
Mateusz Róg <kontakt@matrog.pl>
2026-08-04 16:56:53 +0200

    Updated depth maps

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative overflow-hidden h-screen w-full">
-    <HeroParallax image="/img/hero_1.png" depth-map="/img/hero_depth_map.png">
+    <HeroParallax image="/img/hero_1.png" depth-map="/img/hero-depth-12.png">
       <h1>Test text</h1>
     </HeroParallax>
     <!-- <img src="/img/hero_1.png" class="min-h-screen max-w-max" /> -->
